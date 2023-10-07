62a8792bfb019c90daa7072094de7d4e6cca84e3
Lucas Martins Teixeira <lucaslag.contact@gmail.com>
2023-10-06 21:42:52 -0300

    ui(login): melhora do layout da tela de login

---
+++ src/main/java/com/usjt/firebook/firebook/FireBook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.usjt.firebook.firebook;
+
+/**
+ *
+ * @author Luke
+ */
+public class FireBook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
