059fec999d08da1530e50845092ff11258a065f1
Gabriel Assis <gassis574@gmail.com>
2023-10-29 17:54:57 -0300

    adicao do arquivo operacoes.sql

---
+++ operacoes.sql
@@ -0,0 +1,28 @@
+-- CREATE DATABASE db_firebook
+-- USE db_firebook;
+
+-- INSERT INTO tb_tipo_usuario(tipo_usuario, descricao) VALUES ('1','admin');
+-- INSERT INTO tb_tipo_usuario(tipo_usuario, descricao) VALUES ('2','usuario');
+
+
+--SELECT * FROM tb_usuario;
+-- UPDATE tb_usuario SET tipo_usuario = 1 WHERE tipo_usuario = 2;
+-- UPDATE tb_usuario SET tipo_usuario = 2 WHERE tipo_usuario` = 3;
+
+/*
+INSERT INTO tb_usuario (id_usuario, login, senha, nome) VALUES ('1','teste','dsf123','Lucas');
+INSERT INTO tb_usuario (id_usuario, login, senha, nome) VALUES ('2','teste1','dsf123','Gabriel');
+INSERT INTO tb_usuario (id_usuario, login, senha, nome) VALUES ('3','mariaduarte1','maria456','Maria');
+*/
+
+
+/*
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('1','Biografia');
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('2','Técnico');
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('3','Infantil');
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('4','Romance');
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('5','Ficção');
+INSERT INTO `tb_genero`(`id_genero`, `descricao`) VALUES ('6','Aventura');
+*/
+
+
