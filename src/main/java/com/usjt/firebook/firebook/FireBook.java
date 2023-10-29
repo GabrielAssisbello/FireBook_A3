a027e5eb9346b5274af8c070a4cea6e06809b302
Lucas Martins Teixeira <lucaslag.contact@gmail.com>
2023-10-29 17:05:25 -0300

    entrega3(login): talvez funcione agora

--- src/main/java/com/usjt/firebook/firebook/FireBook.java
+++ src/main/java/com/usjt/firebook/firebook/FireBook.java
@@ -2,7 +2,8 @@ package com.usjt.firebook.firebook;
 
 
 public class FireBook {
-
+    
+    //metodo main
     public static void main(String[] args) {
         LoginTela login = new LoginTela();
         login.setVisible(true);
